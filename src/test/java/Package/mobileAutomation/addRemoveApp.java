02563c4b600ce224725393a1f74520cf28896446
S.K.Obidul kadir <rezwankadirobaidul@gmail.com>
2023-11-11 06:48:38 +0600

    add and remove the apk file.

--- src/test/java/Package/mobileAutomation/addRemoveApp.java
+++ src/test/java/Package/mobileAutomation/addRemoveApp.java
@@ -1,37 +1,49 @@
 package Package.mobileAutomation;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.testng.annotations.Test;
-
-import io.appium.java_client.android.AndroidDriver;
-
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
 
-public class androidApkRun {
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.android.AndroidDriver;
+
+public class addRemoveApp {
+
+	AndroidDriver driver;
 	
-	@Test
-	public void runApp() throws MalformedURLException, InterruptedException {
+	@BeforeTest
+	public void start() throws MalformedURLException {
 		DesiredCapabilities cap = new DesiredCapabilities();
+		File file = new File("./resources/General-Store.apk");
 		cap.setCapability("platformName", "android");
 		cap.setCapability("automationName", "UiAutomator2");
 		cap.setCapability("deviceName", "Pixel");
 		cap.setCapability("udid", "emulator-5554");
-		cap.setCapability("appPackage", "com.androidsample.generalstore");
-		cap.setCapability("appActivity", "com.androidsample.generalstore.SplashActivity");
+		cap.setCapability("app", file.getAbsolutePath());
+		driver = new AndroidDriver(new URL("http://192.168.0.191:4723/"), cap);
+	}
+	
+	@Test
+	public void mainActivityPage() {
 		
-		AndroidDriver driver = new AndroidDriver(new URL("http://192.168.0.191:4723/"), cap);
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.findElement(By.id("com.androidsample.generalstore:id/spinnerCountry")).click();
 		driver.findElement(By.xpath("//android.widget.TextView[@resource-id=\"android:id/text1\" and @text=\"Algeria\"]")).click();
 		
 		driver.findElement(By.xpath("//android.widget.EditText[@resource-id=\"com.androidsample.generalstore:id/nameField\"]")).sendKeys("Hello");
 		driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
-		Thread.sleep(3000);
-	
-		
 	}
-
+	
+	@AfterTest
+	public void removeApp() {
+		driver.removeApp("com.androidsample.generalstore");
+	}
 }
